7e907a38ab5622d3891a8f9072ba05eef48b6b9c
Aleksandr Lukashevich <alukashevi@wiley.com>
2017-08-30 17:43:31 +0300

    add renderer custom

---
+++ src/model/treerenderer/RootCustom.java
@@ -0,0 +1,13 @@
+package model.treerenderer;
+
+public class RootCustom {
+    private String name;
+
+    public RootCustom(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
