943c22da7ae230f9c602c2e2f4e437aa6ffe37b5
Aleksandr Lukashevich <alukashevi@wiley.com>
2017-09-04 13:23:54 +0300

    display stats case type

--- src/view/TestRailWindow.java
+++ src/view/TestRailWindow.java
@@ -8,7 +8,6 @@ import com.intellij.openapi.actionSystem.ActionManager;
 import com.intellij.openapi.actionSystem.DefaultActionGroup;
 import com.intellij.openapi.components.ServiceManager;
 import com.intellij.openapi.project.Project;
-import com.intellij.ui.components.JBLabel;
 import com.intellij.ui.treeStructure.Tree;
 import model.section.OurSection;
 import model.section.OurSectionInflator;
@@ -24,7 +23,6 @@ import javax.swing.*;
 import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.tree.DefaultTreeModel;
 import javax.swing.tree.TreePath;
-import java.awt.*;
 import java.awt.event.ItemEvent;
 import java.util.ArrayList;
 import java.util.List;
@@ -39,6 +37,7 @@ public class TestRailWindow extends WindowPanelAbstract implements Disposable {
     private Tree sectionTree;
     private JLabel loadingLabel;
     private JLabel detailsLabel;
+    private JPanel detailsPanel;
     private RailClient client;
     private ToolWindowData data;
     private List<Case> casesFromSelectedPacks = new ArrayList<>();
@@ -66,6 +65,10 @@ public class TestRailWindow extends WindowPanelAbstract implements Disposable {
 
     public JComboBox getSuitesComboBox() {
         return suitesComboBox;
+    }
+
+    public JPanel getDetailsPanel() {
+        return detailsPanel;
     }
 
     @Override
@@ -119,6 +122,7 @@ public class TestRailWindow extends WindowPanelAbstract implements Disposable {
                     disableComponent(this.projectComboBox);
                     makeVisible(this.loadingLabel);
                     makeInvisible(this.sectionTree);
+                    makeInvisible(this.detailsPanel);
 
                     // Shows section tree.
                     showSectionTree(selectedSuite);
@@ -197,10 +201,10 @@ public class TestRailWindow extends WindowPanelAbstract implements Disposable {
     }
 
     private List<Case> getCasesForSelectedTreeRows() {
-        TreePath[] paths;
+        TreePath[] paths = sectionTree.getSelectionPaths();
         List<Case> casesFromSelectedPacks = new ArrayList<>();
 
-        if (null != (paths = sectionTree.getSelectionPaths())) {
+        if (null != paths) {
 
             for (TreePath path : paths) {
                 Object userObject = ((DefaultMutableTreeNode) path.getLastPathComponent()).getUserObject();
@@ -211,6 +215,9 @@ public class TestRailWindow extends WindowPanelAbstract implements Disposable {
                     casesFromSelectedPacks.addAll(getCases(section));
                 }
             }
+            makeVisible(this.detailsPanel);
+        } else if( null == paths) {
+            makeInvisible(detailsPanel);
         }
         return casesFromSelectedPacks;
     }
@@ -235,10 +242,8 @@ public class TestRailWindow extends WindowPanelAbstract implements Disposable {
 
         }
 
-        makeVisible(this.detailsLabel);
         detailsLabel.setText("<html>" + builder.toString() + "</html>");
         repaintComponent(detailsLabel);
-        //detailsPanel.add(detailsLabel);
     }
     // endregion
 }
